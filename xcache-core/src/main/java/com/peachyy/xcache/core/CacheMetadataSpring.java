5baada1fd811045429f0fcfb3d0deb92c82ad553
peachyy <hixstao@gmail.com>
2020-08-05 11:22:19 +0800

    ADD feature

--- xcache-core/src/main/java/com/peachyy/xcache/core/CacheMetadataSpring.java
+++ xcache-core/src/main/java/com/peachyy/xcache/core/CacheMetadataSpring.java
@@ -1,6 +1,7 @@
 package com.peachyy.xcache.core;
 
 import com.peachyy.xcache.common.CacheMetadata;
+import com.peachyy.xcache.core.exception.CacheException;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
@@ -14,7 +15,7 @@ public class CacheMetadataSpring extends CacheMetadata {
     public static CacheMetadata build(JoinPoint joinPoint){
         Signature signature=joinPoint.getSignature();
         if(!(signature instanceof MethodSignature)){
-           throw new RuntimeException("");
+           throw new CacheException("fail");
         }
         MethodSignature methodSignature= (MethodSignature) signature;
         CacheMetadata cacheMetadata=new CacheMetadata();
